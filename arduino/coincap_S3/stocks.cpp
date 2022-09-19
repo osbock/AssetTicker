5e319d93a2c6c98ee37098ad69bb426f48b69c7a
Kevin Osborn <kevin.osborn@gmail.com>
2022-09-19 12:59:53 -0400

    Change to finnhub api

--- arduino/coincap_S3/stocks.cpp
+++ arduino/coincap_S3/stocks.cpp
@@ -1,7 +1,3 @@
-#include <Arduino.h>
-#include <WiFi.h>
-#include <WiFiMulti.h>
-
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <SPI.h>
@@ -19,7 +15,7 @@ void update_stock(String symbol){
         HTTPClient http;
 
         USE_SERIAL.print("[HTTP] begin...\n");
-        String request = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol="+symbol+"&apikey=" + STOCK_API_KEY;
+        String request = "https://finnhub.io/api/v1/quote?symbol="+symbol+"&token=" + STOCK_API_KEY;
         //Serial.print(request);
         http.begin(request); //HTTP
 
@@ -48,32 +44,27 @@ StaticJsonDocument<512> stockdoc;
 void parse_stock(String payload){
   // String input;
 
-
-
 DeserializationError error = deserializeJson(stockdoc, payload);
-//Serial.println(payload);
+
 if (error) {
   Serial.print("deserializeJson() failed: ");
   Serial.println(error.c_str());
   return;
 }
 
-JsonObject Global_Quote = stockdoc["Global Quote"];
-const char* Global_Quote_01_symbol = Global_Quote["01. symbol"]; // "COF"
-const char* Global_Quote_02_open = Global_Quote["02. open"]; // "101.5400"
-const char* Global_Quote_03_high = Global_Quote["03. high"]; // "101.6200"
-const char* Global_Quote_04_low = Global_Quote["04. low"]; // "98.5300"
-const char* Global_Quote_05_price = Global_Quote["05. price"]; // "100.6500"
-double price = String(Global_Quote_05_price).toFloat();
-Serial.println(price);
+float current_price = stockdoc["c"]; // 101.68
+float d = stockdoc["d"]; // -1.09
+float dp = stockdoc["dp"]; // -1.0606
+float h = stockdoc["h"]; // 102.21
+float l = stockdoc["l"]; // 99.42
+float o = stockdoc["o"]; // 101.56
+float pc = stockdoc["pc"]; // 102.77
+long t = stockdoc["t"]; // 1663358402
+Serial.println(current_price);
 tft.setCursor(60, 125);
 tft.setTextSize(3);
 tft.print("$");
-tft.println(String(price,2));
-const char* Global_Quote_06_volume = Global_Quote["06. volume"]; // "3349982"
-const char* Global_Quote_07_latest_trading_day = Global_Quote["07. latest trading day"]; // "2022-09-14"
-const char* Global_Quote_08_previous_close = Global_Quote["08. previous close"]; // "101.0400"
-const char* Global_Quote_09_change = Global_Quote["09. change"]; // "-0.3900"
-const char* Global_Quote_10_change_percent = Global_Quote["10. change percent"]; // "-0.3860%"
+tft.println(String(current_price,2));
+
 
 }
